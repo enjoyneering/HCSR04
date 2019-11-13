67e382235bfdb04764577e49fb8ec6a6ee897870
enjoyneering <enjoyneering79@gmail.com>
2019-11-13 12:14:09 -0500

    Update HC_SR04_ESP8266_LCD_i2c_Demo.ino

--- examples/HC_SR04_ESP8266_LCD_i2c_Demo/HC_SR04_ESP8266_LCD_i2c_Demo.ino
+++ examples/HC_SR04_ESP8266_LCD_i2c_Demo/HC_SR04_ESP8266_LCD_i2c_Demo.ino
@@ -13,18 +13,6 @@
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/
 
-  This LCD uses I2C bus to communicate, specials pins are required to interface
-  Board:                                    SDA                    SCL                    Level
-  Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5                     5v
-  Mega2560................................. 20                     21                     5v
-  Due, SAM3X8E............................. 20                     21                     3.3v
-  Leonardo, Micro, ATmega32U4.............. 2                      3                      5v
-  Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7    5v
-  Blue Pill, STM32F103xxxx boards.......... PB7                    PB6                    3.3v/5v
-  ESP8266 ESP-01........................... GPIO0/D5               GPIO2/D3               3.3v/5v
-  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1               3.3v/5v
-  ESP32.................................... GPIO21/D21             GPIO22/D22             3.3v
-
 
   Frameworks & Libraries:
   ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
@@ -38,12 +26,7 @@
 */
 /***************************************************************************************************/
 #include <HCSR04.h>
-#include <Wire.h>              //use bug free i2c driver https://github.com/enjoyneering/ESP8266-I2C-Driver
-#include <LiquidCrystal_I2C.h> //https://github.com/enjoyneering/LiquidCrystal_I2C
 #include <ESP8266WiFi.h>
-
-#define LCD_ROWS    4          //qnt. of lcd rows
-#define LCD_COLUMNS 20         //qnt. of lcd columns
 
 float distance = 0;
 
@@ -58,68 +41,47 @@ distance    - maximun measuring distance, in cm
 *if GPIO2/D4 or GPIO0/D3 is used for trigger or echo pin, apply an external
  25kOhm pullup-down resistor otherwise reset & reset button may not work
 */
-HCSR04            ultrasonicSensor(D4, D3, 20, 300);
-LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
-
+HCSR04 ultrasonicSensor(D4, D3, 20, 300);
 
 void setup()
 {
-  WiFi.persistent(false);                                       //disable saving wifi config into SDK flash area
-  WiFi.forceSleepBegin();                                       //disable swAP & station by calling "WiFi.mode(WIFI_OFF)" & put modem to sleep
+  WiFi.persistent(false);   //disable saving wifi config into SDK flash area
+  WiFi.forceSleepBegin();   //disable swAP & station by calling "WiFi.mode(WIFI_OFF)" & put modem to sleep
 
   Serial.begin(115200);
 
-  /* LCD connection check */  
-  while (lcd.begin(LCD_COLUMNS, LCD_ROWS, LCD_5x8DOTS) != true) //20x4 display, 5x8 pixels size
-  {
-    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal."));
-    delay(5000);
-  }
-
-  lcd.print(F("PCF8574 is OK")); //(F()) saves string to flash & keeps dynamic memory free
-  delay(1000);
-
-  lcd.clear();
-
-  ultrasonicSensor.begin();      //set trigger as output & echo pin as input
-
-  /* prints static text */
-  lcd.setCursor(0, 0);
-  lcd.print(F("D :"));
-
-  lcd.setCursor(0, 1);
-  lcd.print(F("Df:"));
+  ultrasonicSensor.begin(); //set trigger as output & echo pin as input
 }
 
 void loop()
 {
   distance = ultrasonicSensor.getDistance();
 
-  lcd.setCursor(3, 0);
   if (distance != HCSR04_OUT_OF_RANGE)
   {
-    lcd.print(distance, 1);
-    lcd.print(F(" cm         "));
+    Serial.print(distance, 1);
+    Serial.println(F(" cm"));                            //(F()) saves string to flash & keeps dynamic memory free
   }
   else
   {
-    lcd.print(F("out of range"));
+    Serial.println(F("out of range"));
   }
   delay(50);                                             //wait 50msec or more, until echo from previous measurement disappears
 
 
   distance = ultrasonicSensor.getMedianFilterDistance(); //pass 3 measurements through median filter, better result on moving obstacles
-  
-  lcd.setCursor(3, 1);
+
   if (distance != HCSR04_OUT_OF_RANGE)
   {
-    lcd.print(distance, 1);
-    lcd.print(F(" cm         "));
+    Serial.print(distance, 1);
+    Serial.println(F(" cm, filtered"));
   }
   else
   {
-    lcd.print(F("out of range"));
+    Serial.println(F("out of range, filtered"));
   }
 
-  delay(250);                                            //screen refresh rate
+  ultrasonicSensor.setTemperature(18.5);                 //set air temperature to compensate change in speed of sound
+
+  delay(250);                                            //serial refresh rate
 }
